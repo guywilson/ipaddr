14db7afd1190b840a4a075bc3a5c1dca1b0a52ef
Guy Wilson <wilson.guy@gmail.com>
2019-10-19 23:20:50 +0100

    Updated script

--- ipaddr.sh
+++ ipaddr.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-curl https://www.ipchicken.com | grep -E -o "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" > ip.addr
+git pull
 
+curl https://www.ipchicken.com | grep -E -o "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" > current_ip.addr
+
+cmp -s ip.addr current_ip.addr
+
+if [ $? -ne 0 ] ; then
+    echo Files are different, uploading...
+    cp current_ip.addr ip.addr
+    rm current_ip.addr
+    git commit -m "Updated IP Address"
+    git push origin master
+fi
