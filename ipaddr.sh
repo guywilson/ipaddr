236e022290366e57308344f8043ffcea829e1bc3
Guy Wilson <wilson.guy@gmail.com>
2019-10-20 10:57:04 +0100

    Updated script

--- ipaddr.sh
+++ ipaddr.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 
+ACTION='\033[1;90m'
+FINISHED='\033[1;96m'
+READY='\033[1;92m'
+NOCOLOR='\033[0m' # No Color
+ERROR='\033[0;31m'
+
+TIMESTAMP=$(date)
+
+echo
+echo -e ${ACTION}Running ipaddr script at ${TIMESTAMP}${NOCOLOR}
+
 git pull
 
 curl -s https://www.ipchicken.com | grep -E -o "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" > current_ip.addr
 
+while IFS= read -r ipaddress; do
+    echo -e ${ACTION}Found IP address: $ipaddress${NOCOLOR}
+done < "current_ip.addr"
+
 cmp -s ip.addr current_ip.addr
 
 if [ $? -ne 0 ] ; then
-    echo Files are different, uploading...
+    echo -e ${ACTION}Files are different, uploading...${NOCOLOR}
     cp current_ip.addr ip.addr
-    rm current_ip.addr
     git commit -m "Updated IP Address"
     git push origin master
 fi
+
+rm current_ip.addr
